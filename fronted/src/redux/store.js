e86e524e9a588b86a67e3a8103826c55b1e7e924
Yerkhan <erhandzoldasov45@gmail.com>
2025-03-22 20:18:48 +0500

    Initialize Redux,add and display books

---
+++ fronted/src/redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit'
+import booksReducer from './books/reducer'
+const store = configureStore({
+  reducer: { books: booksReducer },
+})
+export default store
