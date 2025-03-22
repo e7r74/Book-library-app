807cad1a5cb13ea11c0cdb4f7ece87eb06fd576f
Yerkhan <erhandzoldasov45@gmail.com>
2025-03-22 22:23:41 +0500

    Add Random Book

--- fronted/src/components/BookFrom/BookFrom.js
+++ fronted/src/components/BookFrom/BookFrom.js
@@ -2,11 +2,23 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addBook } from '../../redux/books/actionCreators'
 import { v4 as uuidv4 } from 'uuid'
+import booksData from '../../data/books.json'
 import './BookFrom.css'
 const BookForm = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const dispatch = useDispatch()
+
+  const handleAddRandomBook = () => {
+    const randomIndex = Math.floor(Math.random() * booksData.length)
+    const randomBook = booksData[randomIndex]
+    const randomBookWithID = {
+      ...randomBook,
+      id: uuidv4(),
+    }
+    dispatch(addBook(randomBookWithID))
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (title && author) {
@@ -44,6 +56,9 @@ const BookForm = () => {
           />
         </div>
         <button type="submit">Add Book</button>
+        <button type="button" onClick={handleAddRandomBook}>
+          Add Random
+        </button>
       </form>
     </div>
   )
--- fronted/src/components/BookList/Booklist.js
+++ fronted/src/components/BookList/Booklist.js
@@ -20,8 +20,7 @@ const Booklist = () => {
           {books.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
-                {++i}. {book.title}by
-                <strong>{book.author}</strong>
+                {++i}. {book.title} by <strong>{book.author}</strong>
               </div>
               <div className="book-actions">
                 <button onClick={() => handleDelete(book.id)}>Delete</button>
