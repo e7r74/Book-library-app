6fede39bff312be394911c6a74b2f12826bb0ad2
Yerkhan <erhandzoldasov45@gmail.com>
2025-04-12 22:43:58 +0500

    Added errorSlice

--- fronted/src/redux/store.js
+++ fronted/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 // import booksReducer from './books/reducer'
 import booksReducer from './slices/bookSlice'
 import filterSlice from './slices/filterSlice'
+import errorReducer from './slices/errorSlice'
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     filter: filterSlice,
+    error: errorReducer,
   },
 })
 export default store
